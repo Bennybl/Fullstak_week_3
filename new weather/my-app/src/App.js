670f5c2da28f25666ebf9e155f224ad25222215a
Benny Blaustein <benny.b@brandshield.com>
2023-04-30 22:16:10 +0300

    class Component

--- new weather/my-app/src/App.js
+++ new weather/my-app/src/App.js
@@ -1,51 +1,61 @@
-import React, { useState } from "react";
 import { data } from "./AppSetting";
 import axios from "axios";
-function SearchBar(props) {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [searchRes, setSearchRes] = useState(null);
+import React, { Component } from 'react';
 
-  const handleSearchInputChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleSearchButtonClick = () => {
-    // Do something with the search term, such as fetching search results
-    getWeather(searchTerm);
-  };
-
-
-  const getWeather = (city) => {
-    const params = {'q': city, 'units': 'metric', 'appid': data.WEATHER_TOKEN }
-	  axios.get(data.WEATHER_URL, { params })
-  .then(response => {
-    setSearchRes(response.data)
-  })
-  .catch(error => {
-    console.error(error);
-  });
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      searchRes: null,
+    };
   }
 
-  return (
-    <div>
-      <input
-           type="text"
-           value={searchTerm}
-           onChange={handleSearchInputChange}
-           placeholder="Search..."
-         />
-         <button onClick={handleSearchButtonClick}>Search</button>
-         {searchRes && 
-        // <pre>{JSON.stringify(searchRes, null, 2)}</pre>
-        <div>
+  handleSearchInputChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  handleSearchButtonClick = () => {
+    // Do something with the search term, such as fetching search results
+    this.getWeather(this.state.searchTerm);
+  };
+
+  getWeather = (city) => {
+    const params = { q: city, units: 'metric', appid: data.WEATHER_TOKEN };
+    axios
+      .get(data.WEATHER_URL, { params })
+      .then((response) => {
+        this.setState({ searchRes: response.data });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  render() {
+    const { searchTerm, searchRes } = this.state;
+
+    return (
+      <div>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={this.handleSearchInputChange}
+          placeholder="Search..."
+        />
+        <button onClick={this.handleSearchButtonClick}>Search</button>
+        {searchRes && (
+          <div>
             <div>{`Location is ${searchRes.name}, ${searchRes.sys.country}`}</div>
             <div>{` temperature is: ${searchRes.main.temp}`}</div>
             <div>{` description: ${searchRes.weather[0].description}`}</div>
             <div>{` temperature feels like: ${searchRes.main.feels_like}`}</div>
-        </div>
-      }
-    </div>
-  );
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
+
